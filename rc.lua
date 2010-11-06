f8d90a7c8de78573d75a6fbd668bf063052e8a86
Sergey Mironov <ierton@gmail.com>
2010-11-06 22:51:05 +0300

    rc.lua: move run_or_raise to the beginning of file

--- rc.lua
+++ rc.lua
@@ -49,6 +49,66 @@ function dbg_client(c)
 end
 --}}}
 
+--{{{ Run or raise
+--- Spawns cmd if no client can be found matching properties
+-- If such a client can be found, pop to first tag where it is visible, and give it focus
+-- @param cmd the command to execute
+-- @param properties a table of properties to match against clients.  Possible entries: any properties of the client object
+function run_or_raise(cmd, properties)
+    local clients = client.get()
+    local focused = awful.client.next(0)
+    local findex = 0
+    local matched_clients = {}
+    local n = 0
+
+    -- Returns true if all pairs in table1 are present in table2
+    function match (table1, table2)
+        for k, v in pairs(table1) do
+            if table2[k] ~= v and not table2[k]:find(v) then
+                return false
+            end
+        end
+        return true
+    end
+
+    for i, c in pairs(clients) do
+        --make an array of matched clients
+        if match(properties, c) then
+            n = n + 1
+            matched_clients[n] = c
+            if c == focused then
+                findex = n
+            end
+        end
+    end
+    if n > 0 then
+        local c = matched_clients[1]
+        -- if the focused window matched switch focus to next in list
+        if 0 < findex and findex < n then
+            c = matched_clients[findex+1]
+        end
+        local ctags = c:tags()
+        if table.getn(ctags) == 0 then
+            -- ctags is empty, show client on current tag
+            local curtag = awful.tag.selected()
+            awful.client.movetotag(curtag, c)
+        else
+            -- Otherwise, pop to first tag client is visible on
+            awful.tag.viewonly(ctags[1])
+        end
+        -- And then focus the client
+        if client.focus == c then
+            c:tags({})
+        else
+            client.focus = c
+            c:raise()
+        end
+        return
+    end
+    awful.util.spawn(cmd, false)
+end
+--}}}
+
 --{{{ Data serialisation helpers
 function client_name(c)
     local cls = c.class or ""
@@ -607,64 +667,6 @@ function toggle_tag(name)
         end
         awful.tag.viewonly(t)
     end
-end
-
---- Spawns cmd if no client can be found matching properties
--- If such a client can be found, pop to first tag where it is visible, and give it focus
--- @param cmd the command to execute
--- @param properties a table of properties to match against clients.  Possible entries: any properties of the client object
-function run_or_raise(cmd, properties)
-    local clients = client.get()
-    local focused = awful.client.next(0)
-    local findex = 0
-    local matched_clients = {}
-    local n = 0
-
-    -- Returns true if all pairs in table1 are present in table2
-    function match (table1, table2)
-        for k, v in pairs(table1) do
-            if table2[k] ~= v and not table2[k]:find(v) then
-                return false
-            end
-        end
-        return true
-    end
-
-    for i, c in pairs(clients) do
-        --make an array of matched clients
-        if match(properties, c) then
-            n = n + 1
-            matched_clients[n] = c
-            if c == focused then
-                findex = n
-            end
-        end
-    end
-    if n > 0 then
-        local c = matched_clients[1]
-        -- if the focused window matched switch focus to next in list
-        if 0 < findex and findex < n then
-            c = matched_clients[findex+1]
-        end
-        local ctags = c:tags()
-        if table.getn(ctags) == 0 then
-            -- ctags is empty, show client on current tag
-            local curtag = awful.tag.selected()
-            awful.client.movetotag(curtag, c)
-        else
-            -- Otherwise, pop to first tag client is visible on
-            awful.tag.viewonly(ctags[1])
-        end
-        -- And then focus the client
-        if client.focus == c then
-            c:tags({})
-        else
-            client.focus = c
-            c:raise()
-        end
-        return
-    end
-    awful.util.spawn(cmd, false)
 end
 
 function chord_mpd()
